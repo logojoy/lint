577b743ff178eb50bfcfe1f31349ea9987253505
Yazan Medanat <yazan@logojoy.com>
2018-07-20 14:48:22 -0400

    Adds eslint-config and eslint-config react packages.

---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+    parser: 'babel-eslint',
+    plugins: [
+      'react'
+    ],
+    env: {
+      browser: true,
+      node: true,
+      mocha: true
+    },
+    extends: ['plugin:react/recommended'],
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    rules: {}
+};
