2677d65c6e9ecdba2edb35eff89f4cf2d40dfdf3
Yazan Medanat <yazan@logojoy.com>
2018-09-12 15:37:10 -0400

    Adds prefer const and keyword spacing rules.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -11,7 +11,9 @@ module.exports = {
     rules: {
       'array-bracket-spacing': ['error', 'never'],
       'eol-last': ['error', 'always'],
+      'keyword-spacing': ['error'],
       'object-curly-spacing': ['error', 'always'],
+      'prefer-const': ['error'],
       semi: ['error', 'always'],
       'space-before-blocks': ['error', 'always'],
       quotes: ['error', 'single']
