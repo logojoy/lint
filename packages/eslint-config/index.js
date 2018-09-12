180399f892f88bd8b73d3eed7a988486271d7563
Yazan Medanat <yazan@logojoy.com>
2018-09-12 15:29:03 -0400

    Adds eol and quotes rule.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -10,8 +10,10 @@ module.exports = {
     },
     rules: {
       'array-bracket-spacing': ['error', 'never'],
+      'eol-last': ['error', 'always'],
       'object-curly-spacing': ['error', 'always'],
       semi: ['error', 'always'],
-      'space-before-blocks': ['error', 'always']
+      'space-before-blocks': ['error', 'always'],
+      quotes: ['error', 'single']
     }
 };
