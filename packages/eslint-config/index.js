d488f2eff7f2488dd1a6bbaaffad107b212982ae
Yazan Medanat <yazan@logojoy.com>
2019-01-21 16:54:26 -0500

    Introduces new base and react eslint rules.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,20 +4,25 @@ module.exports = {
         mocha: true,
         es6: true
     },
-    extends: 'eslint:recommended',
+    extends: ['eslint:recommended'],
     parserOptions: {
         ecmaVersion: 2018
     },
     rules: {
         'array-bracket-spacing': ['error', 'never'],
         'eol-last': ['error', 'always'],
-        'indent': ['error', 4],
+        eqeqeq: ['error'],
+        indent: ['error', 4],
+        'jsx-quotes': ['error', 'prefer-single'],
         'keyword-spacing': ['error'],
         'no-tabs': ['error'],
+        'no-trailing-spaces': ['error'],
         'object-curly-spacing': ['error', 'always'],
         'prefer-const': ['error'],
+        'prefer-template': ['error'],
         semi: ['error', 'always'],
         'space-before-blocks': ['error', 'always'],
+        'quote-props': ['error', 'as-needed'],
         quotes: ['error', 'single']
     }
 };
